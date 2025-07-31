97785bfa6b9b2c367a649e37da5d182180ccddfe
Joshua Weber <josh@joshuaw.de>
2025-07-31 15:18:13 +0200

    updates tap to newest version

--- billboard.rb
+++ billboard.rb
@@ -1,14 +1,14 @@
 class Billboard < Formula
   desc "Show text in your Mac menu bar"
   homepage "https://github.com/daschaa/billboard"
-    version "1.0.0"
+    version "1.3.0"
 
     on_macos do
       if Hardware::CPU.intel?
-        url "https://github.com/daschaa/billboard/releases/download/v1.0.0/billboard-x86_64.zip"
+        url "https://github.com/daschaa/billboard/releases/download/v1.3.0/billboard-x86_64.zip"
         sha256 "4310d51b38b108e4709c348c29ed8ae4dcf8786f8fdc903932fa91fe217444bf"
       elsif Hardware::CPU.arm?
-        url "https://github.com/daschaa/billboard/releases/download/v1.0.0/billboard-arm64.zip"
+        url "https://github.com/daschaa/billboard/releases/download/v1.3.0/billboard-arm64.zip"
         sha256 "cebfa25d3463c17c3e85cdead2aeeaa3bdb5a92022232c5aa36e96ec107f553f"
       end
     end
